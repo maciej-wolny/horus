ea91d68e7d3d4814e408722251a72a8ec7a3ead3
maciej.wolny <maciej.wolny@olx.pl>
2021-10-04 04:06:32 +0200

    fixes

--- video_recorder.py
+++ video_recorder.py
@@ -1,4 +1,6 @@
+import queue
 import time
+from threading import Thread
 
 import cv2
 import os
@@ -18,22 +20,31 @@ class BirdDetectionVideoHandler:
         self.frame_width = frame_width
         self.frame_height = frame_height
         self.current_file = None
+        self.frames_queue = queue.Queue()
         if not os.path.exists(RECORDINGS_DIRECTORY):
             os.makedirs(RECORDINGS_DIRECTORY)
 
     def new_recording(self, recording_id):
         print("new recording:" + recording_id)
+
         self.current_file = RECORDINGS_DIRECTORY + recording_id + ".avi"
         self.writer = cv2.VideoWriter(self.current_file, cv2.VideoWriter_fourcc('M', 'J', 'P', 'G'),
                                       FRAME_RATE,
                                       (self.frame_width, self.frame_height))
         self.recording = True
+        Thread(target=self._write_loop, args=()).start()
+
+    def _write_loop(self):
+        while self.recording:
+            while not self.frames_queue.empty():
+                data = self.frames_queue.get()
+                print("pop frame")
+                while data['frames'] > 0:
+                    self.writer.write(data['frame'])
+                    data['frames'] -= 1
 
     def write_frame(self, frame, current_fps):
-        i = 0
-        while i < (FRAME_RATE / current_fps):
-            self.writer.write(frame)
-            i += 1
+        self.frames_queue.put({"frame": frame, "frames": FRAME_RATE / current_fps})
 
     def bird_detected(self):
         self.last_bird_occurrence = time.time()
